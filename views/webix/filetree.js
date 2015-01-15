be8aea6e476f12a2ecbc51fa6cca585c2313fe43
Maria-Staskova <mary@xbsoftware.com>
2015-01-15 18:22:18 +0300

    [add] webix libs

--- views/webix/filetree.js
+++ views/webix/filetree.js
@@ -4,7 +4,6 @@ webix.type(webix.ui.tree, {
 	height: 40,
 	templateCommon:webix.template("{common.icon()} <div class='folder_title'>{common.folder()} #value#</div>"),
 	folder:function(obj, common){
-
 		return "<span class='webix_icon icon fa-folder"+(obj.open?"-open":(obj.$level>3?"-o":""))+"'></span>";
 	}
 });
