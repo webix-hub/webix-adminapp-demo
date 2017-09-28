a467ac0832974cf0951149affec45714cf68945a
Maksim Kozhukh <mkozhukh@ya.ru>
2017-09-28 16:51:33 +0300

    [dev] correct babel configuration is applied

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,54 @@
-var path = require('path');
+var path = require("path");
+var webpack = require("webpack");
 
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
+module.exports = function(env) {
 
-var config = {
-  entry: './sources/admin.js',
-  output: {
-    path: path.join(__dirname, 'codebase'),
-    publicPath:"/codebase/",
-    filename: 'admin.js'
-  },
-  devtool: 'inline-source-map',
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        loader: 'babel-loader'
-      },
-      {
-      	test: /\.(svg|png|jpg|gif)$/,
-      	loader: 'url-loader?limit=25000',
-         options: {
-          presets: ['env', { loose:true }]
-        }
-      },
-      {
-        test: /\.less$/,
-        loader: ExtractTextPlugin.extract("css-loader!less-loader")
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['.js'],
-    modules: ["./sources", "node_modules", "bower_components"],
-    alias:{
-      "jet-views":path.resolve(__dirname, 'sources/views'),
-      "jet-locales":path.resolve(__dirname, 'sources/locales')
-    }
-  },
-  plugins: [
-    new ExtractTextPlugin("./admin.css")
-  ]
-};
+	var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-module.exports = config;
+	var config = {
+		entry: "./sources/admin.js",
+		output: {
+			path: path.join(__dirname, "codebase"),
+			publicPath:"/codebase/",
+			filename: "admin.js"
+		},
+		devtool: "inline-source-map",
+		module: {
+			rules: [
+				{
+					test: /\.js$/,
+					loader: "babel-loader"
+				},
+				{
+					test: /\.(svg|png|jpg|gif)$/,
+					loader: "url-loader?limit=25000"
+				},
+				{
+					test: /\.less$/,
+					loader: ExtractTextPlugin.extract("css-loader!less-loader")
+				}
+			]
+		},
+		resolve: {
+			extensions: [".js"],
+			modules: ["./sources", "node_modules", "bower_components"],
+			alias:{
+				"jet-views":path.resolve(__dirname, "sources/views"),
+				"jet-locales":path.resolve(__dirname, "sources/locales")
+			}
+		},
+		plugins: [
+			new ExtractTextPlugin("./admin.css")
+		]
+	};
+
+	if (env && env.compress === "true") {
+		config.plugins.push(
+			new  webpack.optimize.UglifyJsPlugin({
+				test: /\.js$/
+			})
+		);
+	}
+
+	return config;
+}
